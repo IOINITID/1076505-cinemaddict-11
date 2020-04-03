20960375bd12e9440e69f06b3e9c86ce81124ee3
IOINITID <50173108+IOINITID@users.noreply.github.com>
2020-04-03 16:13:39 +0300

    I broke the components into modules, connected them in one file and completed the build setup using webpack

---
+++ src/components/show-more-button.js
@@ -0,0 +1,6 @@
+// Возвращает разметку кнопки показать еще
+export const createShowMoreButton = () => {
+  return (
+    `<button class="films-list__show-more">Show more</button>`
+  );
+};
---
+++ src/components/site-footer-statistics.js
@@ -0,0 +1,6 @@
+// Возвращает разметку блока статистика
+export const createSiteFooterStatisctics = () => {
+  return (
+    `<p>130 291 movies inside</p>`
+  );
+};
