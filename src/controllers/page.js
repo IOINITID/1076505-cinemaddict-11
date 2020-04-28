7911b707207563ff6f2eb89c215920ad86a78d69
IOINITID <50173108+IOINITID@users.noreply.github.com>
2020-04-28 23:40:22 +0300

    Fixed display cards

--- src/controllers/page.js
+++ src/controllers/page.js
@@ -183,7 +183,28 @@ export default class PageController {
 
       renderFilms(filmsListContainer, sortedFilms);
 
-      renderShowMoreButton();
+      const sortElements = this._sortComponent.getElement().querySelectorAll(`.sort__button`);
+
+      const clearActiveState = () => {
+        sortElements.forEach((element) => {
+          element.classList.remove(`sort__button--active`);
+        });
+      };
+
+      const onSortElementClick = (element) => {
+        element.addEventListener(`click`, () => {
+          clearActiveState();
+          element.classList.add(`sort__button--active`);
+        });
+      };
+
+      sortElements.forEach((element) => {
+        onSortElementClick(element);
+      });
+
+      if (!this._showMoreButtonComponent) {
+        renderShowMoreButton();
+      }
     });
   }
 }
